d8e5721c9b12648ab879d4dd544bebb5a0d8b6ec
Priyanka Singh Tomer <136883301+PST09@users.noreply.github.com>
2023-08-28 16:38:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function getSize(){
+	let height=document.documentElement.clientHeight;
+	let width=document.documentElement.clientWidth;
+	document.getElementById("sizeInfo").innerHTML="<h1>Width: " +width+" and Height: "+height+"</h1>";
+}
